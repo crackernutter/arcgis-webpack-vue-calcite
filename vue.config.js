7f693336263ec35eb8c6c69ce0601255dd65f099
Gregory <Jay.CTR.Gregory@faa.gov>
2019-06-27 09:45:47 -0400

    split chunks added to webpack config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 const ArcGISPlugin = require("@arcgis/webpack-plugin");
 const path = require('path');
+const webpack = require('webpack');
 module.exports = {
   
     //runtimeCompiler: false
@@ -63,6 +64,13 @@ module.exports = {
             global: false,
             //fs: "empty"
           },
+          optimization: {
+            minimize: true,
+            splitChunks: {
+              minChunks:Infinity,
+                chunks: 'all',
+            }
+        }
     },
     publicPath: process.env.NODE_ENV === 'production' ? "/" : '/'
     
